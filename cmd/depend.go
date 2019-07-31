5697e67f21cc77993b697a09a6442f8d748856dd
ANON <673179458@qq.com>
2019-07-31 03:45:12 +0000

    add pkill pls

--- cmd/depend.go
+++ cmd/depend.go
@@ -36,5 +36,14 @@ var getCmd = &cobra.Command{
 			os.Exit(0)
 			return
 		}
+		command3 := exec.Command("pkill", "gopls")
+		command3.Dir = getCurrentAbsPath()
+		command3.Stderr = os.Stderr
+		command3.Stdout = os.Stdout
+		if err := command3.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+			return
+		}
 	},
 }
