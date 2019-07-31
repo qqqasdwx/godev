48587851bc96d7ca647214a83b1a8969bf06e316
ANON <673179458@qq.com>
2019-07-31 11:44:55 +0000

    update

--- cmd/depend.go
+++ cmd/depend.go
@@ -19,8 +19,9 @@ var getCmd = &cobra.Command{
 	Long:  `go get [repo] && go mod vendor -v`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		command1 := exec.Command("docker-compose", "exec", "-T", "workspace", "/bin/sh", "-c", fmt.Sprintf("go get -v %s", args[0]))
+		command1 := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("go get -v %s", args[0]))
 		command1.Dir = fmt.Sprintf("%s/%s", getCurrentAbsPath(), "workspace")
+		command1.Stdin = os.Stdin
 		command1.Stderr = os.Stderr
 		command1.Stdout = os.Stdout
 		if err := command1.Run(); err != nil {
@@ -28,8 +29,9 @@ var getCmd = &cobra.Command{
 			os.Exit(0)
 			return
 		}
-		command2 := exec.Command("docker-compose", "exec", "-T", "workspace", "/bin/sh", "-c", fmt.Sprintf("go mod vendor -v"))
+		command2 := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("go mod vendor -v"))
 		command2.Dir = fmt.Sprintf("%s/%s", getCurrentAbsPath(), "workspace")
+		command2.Stdin = os.Stdin
 		command2.Stderr = os.Stderr
 		command2.Stdout = os.Stdout
 		if err := command2.Run(); err != nil {
@@ -54,8 +56,9 @@ var commonCmd = &cobra.Command{
 	Short: "get common",
 	Long:  `get common && go mod vendor -v`,
 	Run: func(cmd *cobra.Command, args []string) {
-		command1 := exec.Command("docker-compose", "exec", "-T", "workspace", "/bin/sh", "-c", fmt.Sprintf("go get -v %s", "git.secok.com/cad/common"))
+		command1 := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("go get -v %s", "git.secok.com/cad/common"))
 		command1.Dir = fmt.Sprintf("%s/%s", getCurrentAbsPath(), "workspace")
+		command1.Stdin = os.Stdin
 		command1.Stderr = os.Stderr
 		command1.Stdout = os.Stdout
 		if err := command1.Run(); err != nil {
@@ -63,8 +66,9 @@ var commonCmd = &cobra.Command{
 			os.Exit(0)
 			return
 		}
-		command2 := exec.Command("docker-compose", "exec", "-T", "workspace", "/bin/sh", "-c", fmt.Sprintf("go mod vendor -v"))
+		command2 := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("go mod vendor -v"))
 		command2.Dir = fmt.Sprintf("%s/%s", getCurrentAbsPath(), "workspace")
+		command2.Stdin = os.Stdin
 		command2.Stderr = os.Stderr
 		command2.Stdout = os.Stdout
 		if err := command2.Run(); err != nil {
