812190af6f1d2a72952c51d4dbb73e13c2583fec
ANON <673179458@qq.com>
2019-08-01 04:03:08 +0000

    update

--- cmd/run.go
+++ cmd/run.go
@@ -14,10 +14,10 @@ func init() {
 
 var runCmd = &cobra.Command{
 	Use:   "run",
-	Short: "run with go",
-	Long:  `docker-compose exec workspace /bin/sh -c "GOFLAGS=-mod=vendor go run main.go"`,
+	Short: "run with bee",
+	Long:  `docker-compose exec workspace /bin/sh -c "GOFLAGS=-mod=vendor bee run"`,
 	Run: func(cmd *cobra.Command, args []string) {
-		command := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("GOFLAGS=-mod=vendor go run main.go"))
+		command := exec.Command("docker-compose", "exec", "workspace", "/bin/sh", "-c", fmt.Sprintf("GOFLAGS=-mod=vendor bee run"))
 		command.Dir = fmt.Sprintf("%s/%s", getCurrentAbsPath(), "workspace")
 		command.Stdin = os.Stdin
 		command.Stderr = os.Stderr
